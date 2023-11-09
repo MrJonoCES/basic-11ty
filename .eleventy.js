88e62cf495c58394348baf962e7a1e5ae3f58d78
MrJonoCES <mrjonolewarne@gmail.com>
2023-11-09 23:28:05 +0000

    Added bits and bobs from AB course up to lesson 17

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,14 @@
 const {DateTime} = require("luxon"); 
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
 
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
-    
+
+    // Plugins
+    config.addPlugin(rssPlugin);
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
---
+++ src/_data/helpers.js
@@ -0,0 +1,25 @@
+// In this helper function, we pass in an item from navigation.items and the current page URL. We then return back some attributes, depending on state.
+
+module.exports = {
+    /**
+     * Returns back some attributes based on whether the
+     * link is active or a parent of an active item
+     *
+     * @param {String} itemUrl The link in question
+     * @param {String} pageUrl The page context
+     * @returns {String} The attributes or empty
+     */
+    getLinkActiveState(itemUrl, pageUrl) {
+      let response = '';
+  
+      if (itemUrl === pageUrl) {
+        response = ' aria-current="page"';
+      }
+  
+      if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
+        response += ' data-state="active"';
+      }
+  
+      return response;
+    }
+  };  
