1a20e9bd5d17ef95233a9a2906523425422e7bba
MrJonoCES <mrjonolewarne@gmail.com>
2023-11-10 12:08:14 +0000

    Added critical css inline

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,20 @@
 const {DateTime} = require("luxon"); 
 const rssPlugin = require('@11ty/eleventy-plugin-rss');
+const lightningCSS = require("@11tyrocks/eleventy-plugin-lightningcss");
+const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPlugin(lightningCSS);
+    eleventyConfig.addPassthroughCopy('./src/css/style.css');
+    // eleventyConfig.addPassthroughCopy('./src/css/reset.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
+    
+    // inline-css
+    eleventyConfig.addFilter("cssmin", function(code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
 
     // Plugins
     eleventyConfig.addPlugin(rssPlugin);
